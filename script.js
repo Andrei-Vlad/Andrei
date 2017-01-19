8d12b34f4122ab098b4eb8567581be36557ee2c5
Andrei-Vlad <cerghivlad@yahoo.com>
2017-01-19 22:18:44 +0200

    Hide green

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log("Hello World!");
+$(document).ready(function(){
+
+	$(".vlad").mouseenter(function() {
+		$(".andrei").hide() 
+	});
 	
+	$(".vlad").mouseleave(function() {
+		$(".andrei").show();	
+	});
+
+
+
+
+});
