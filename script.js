8cf5234317878040b65f5e6c93812b16638647b6
Andrei-Vlad <cerghivlad@yahoo.com>
2017-01-19 22:27:02 +0200

    Revert "Hide green"
    This reverts commit 8d12b34f4122ab098b4eb8567581be36557ee2c5.

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
 console.log("Hello World!");
-$(document).ready(function(){
-
-	$(".vlad").mouseenter(function() {
-		$(".andrei").hide() 
-	});
 	
-	$(".vlad").mouseleave(function() {
-		$(".andrei").show();	
-	});
-
-
-
-
-});
