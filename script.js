38f8498607b5bb3439ea324d8c21f9fc194a69d5
Andrei Vlad <cerghivlad@yahoo.com>
2017-01-31 16:52:06 +0200

    am adaugat div portocaliu

--- script.js
+++ script.js
@@ -17,6 +17,24 @@ $(document).ready(function(){
 				{  	
 						$(".vlad").css("background-color","red");
 				});
+	$('#portocaliu').hover(function(){
+		$(this).text('hello');
+	});
+
+
+	$("input").change(function(){
+		console.log("change")
+		$("#para").html( string_chop($("#text").val(), $("#number").val()) );
+	});
 });
 
+function string_chop(text, number){
+	console.log(text, number)
+	a=text.length/2;
+	b=number/2;
+	c=text.slice(a-b, a+b);
+	return(c);
 
+};
+
+ 
