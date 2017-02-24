acaf87a10d129d56c152c910560d51a4845212fd
Andrei-Vlad <cerghivlad@yahoo.com>
2017-02-25 00:00:12 +0200

    schimbari nesalvate la fisierle proiectului 1

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ $(document).ready(function(){
 		$(this).text('hello');
 	});
 
-
 	$("input").change(function(){
 		console.log("change")
 		$("#para").html( string_chop($("#text").val(), $("#number").val()) );
 	});
 
-
-	
 	$(".valori").change(function(){
 		console.log("change")
 		$("#img").css("top", $('#y').val()+ "px")
 			     .css("left", $('#x').val()+ "px")
 			     .css("transform", "rotate("+ $('#r').val()+"deg)");
+
+	console.log("img")
+
 	});
 });
 
